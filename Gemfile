7b4b7b3458c77c6c947e12d8170ddfedeadf4cab
stefano amodei <samodei@udesa.edu.ar>
2023-05-25 23:10:54 -0300

    Pg_search multisearch testing

--- Gemfile
+++ Gemfile
@@ -75,3 +75,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+
+gem 'pg_search', '~> 2.3', '>= 2.3.6'
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,13 +2,17 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     if params[:query].present?
-      sql_subquery = <<~SQL
-        movies.title @@ :query
-        OR movies.synopsis @@ :query
-        OR directors.first_name @@ :query
-        OR directors.last_name @@ :query
-      SQL
-      @movies = @movies.joins(:director).where(sql_subquery, query: params[:query])
+      # sql_subquery = <<~SQL
+      #   movies.title @@ :query
+      #   OR movies.synopsis @@ :query
+      #   OR directors.first_name @@ :query
+      #   OR directors.last_name @@ :query
+      # SQL
+      # @movies = @movies.joins(:director).where(sql_subquery, query: params[:query])
+
+      # @movies = Movie.search_by_title_and_synopsis(params[:query])
+
+      @results = PgSearch.multisearch(params[:query])
     end
   end
 end
